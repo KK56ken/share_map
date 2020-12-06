28b4533f6a890632f9e9779812240ba739aef659
Ksub77 <kiido1612@icloud.com>
2020-12-06 16:13:54 +0900

    [FIX]remake_directory

---
+++ app/server.py
@@ -0,0 +1,24 @@
+import tornado.ioloop
+import tornado.web
+import os
+import sys
+import folium
+
+class MainHandler(tornado.web.RequestHandler):
+  def get(self):
+    self.render("tokyo.html")
+
+application = tornado.web.Application([
+  (r"/",MainHandler),
+],
+  template_path=os.path.join(os.getcwd(),"templates")
+)
+
+def create_map():
+  tokyo = folium.Map(location=[36, 140],zoom_start=4)
+  tokyo.save('/app/templates/tokyo.html')
+
+if __name__ == "__main__":
+  create_map()
+  application.listen(8888)
+  tornado.ioloop.IOLoop.instance().start()
