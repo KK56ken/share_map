50baf02b8217687fd07583cf7c04c4440bdc7f22
tama2135 <tasuku.tamada@gmail.com>
2020-12-12 17:11:45 +0900

    add pin

--- app/server.py
+++ app/server.py
@@ -16,7 +16,8 @@ application = tornado.web.Application([
 
 def create_map():
   tokyo = folium.Map(location=[36, 140],zoom_start=4)
-  tokyo.save('/app/templates/tokyo.html')
+  folium.Marker([36, 140], popup='tokyo').add_to(tokyo)
+  tokyo.save('../app/templates/tokyo.html')
 
 if __name__ == "__main__":
   create_map()
